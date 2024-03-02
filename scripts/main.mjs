6ee052263ac4bf42901c0d83b2ac3017b96d2c14
osman <osmanlyumanov2005@gmail.com>
2024-03-02 18:42:30 +0300

    the first version of the project

---
+++ scripts/main.mjs
@@ -0,0 +1,16 @@
+// const clientsHeading = prompt("Здравствуй, путник! Как Вас зовут?");
+// if (clientsHeading !== null) {
+// 	const heading = document.querySelector("main p");
+// 	heading.textContent = `Веселый ${clientsHeading}`;
+// }
+
+// document.querySelector("html").onclick = () =>
+// 	alert("Что Вы себе позволяете?! Прекратите сейчас же!");
+
+document.querySelector("main div img").ondblclick = () =>
+	alert("Щекотно! Хи-хи-хи");
+
+// document.querySelector("html").ondblclick = () => alert("Прекратите СЕЙЧАС!");
+
+// const clientsName = prompt("Как Вас зовут, странник?");
+// localStorage.setItem("name", clientsName);
