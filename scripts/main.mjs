f4070ad889320b5ff5a9083fd8904310f6e85702
osman <osmanlyumanov2005@gmail.com>
2024-03-12 21:23:21 +0300

    the changes have been made

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -7,10 +7,21 @@
 // document.querySelector("html").onclick = () =>
 // 	alert("Что Вы себе позволяете?! Прекратите сейчас же!");
 
-document.querySelector(".profile").ondblclick = () =>
-	alert("Щекотно! Хи-хи-хи");
+document.querySelector("main").ondblclick = () => alert("Щекотно! Хи-хи-хи");
 
 // document.querySelector("html").ondblclick = () => alert("Прекратите СЕЙЧАС!");
 
 // const clientsName = prompt("Как Вас зовут, странник?");
 // localStorage.setItem("name", clientsName);
+
+// document.querySelector(".nav-show").onclick = () => {
+// 	const nav = document.querySelector("nav");
+// 	const navShowBtn = document.querySelector(".nav-show");
+// 	if (nav.style.display === "none") {
+// 		nav.style.display = "flex";
+// 		navShowBtn.style.top = "70px";
+// 	} else {
+// 		nav.style.display = "none";
+// 		navShowBtn.style.top = "15px";
+// 	}
+// };
